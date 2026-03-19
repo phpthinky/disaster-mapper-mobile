9352d3803421eadb4bc85c2dc2aab37339fef2d3
Harold Rita <letswrite14@gmail.com>
2026-03-19 21:05:48 +0800

    Create report page forms and geolocation current location;

--- resources/js/Pages/Report.jsx
+++ resources/js/Pages/Report.jsx
@@ -1,10 +1,171 @@
+import { useState } from 'react';
 import MobileLayout from '../Layouts/MobileLayout';
 
 export default function Report() {
+    const [form, setForm] = useState({
+        incident_type:'',
+        severity:'',
+        barangay:'',
+        description:'',
+        radius:'',
+        latitude:'',
+        longitude:'',
+    });
+
+    const incidentTypes = [
+        'Flooding',
+        'Landslide',
+        'Strong Winds',
+        'Damage House',
+        'Road Blocked',
+        'Missing person',
+        'Medical Emergency',
+        'Fire',
+        'Other',
+        ];
+    const barangays = [
+        'Arellano',
+        'Batong Buhay',
+        'Buenavista',
+        'Burgos',
+        'Claudio Salgado',
+        'Gen. Emilio Aguinaldo',
+        'Ligaya',
+        'Paetan',
+        'Poblacion',
+        'San Agustin',
+        'San Vicente',
+        'Santo Niño',
+        'Sta. Lucia',
+        'San Nicolas',
+        'Tuban',
+        ];
+    const severities =[
+        'Minor',
+        'Major',
+        'Critical',
+        ];
+    const handleChange = (field,value) =>{
+        setForm({...form, [field]:value});
+    };
+
+
+    const geoLocation = () =>{
+        navigator.geolocation.getCurrentPosition(pos =>{
+            handleChange('latitude',post.coords.latitude);
+            handleChange('longitude',post.coords.longitude);
+        });
+    };
+
+    const handleSubmit = () =>{
+        console.log('Report submitted:',form);
+        alert('Report submitted successfully');
+    }
+
+
     return (
         <MobileLayout title="File Report" active="report">
-            <div className="p-4">
-                <p className="text-gray-500">Report screen coming soon...</p>
+            <div className="p-4 flex flex-col gap-4">
+                {/*Incident Types*/}
+                <div className="bg-white rounded-xl p-4 shadow">
+                    <label className="text-xs uppercase tracking-wide">Incident Type</label>
+                    <select
+                        className="w-full mt-2 p-2 border border-gray-200 rounded-lg text-sm"  
+                        value={form.incident_type}
+                        onChange={e => handleChange('incident_type',e.target.value)}>
+                    <option value ="">Select incident type..</option>
+                    {incidentTypes.map(type => (
+                        <option key={type} value={type}>{type}</option>
+                        ))}
+                    </select>
+                </div>
+
+                {/*Severity*/}
+                <div className="bg-white rounded-xl p-4 shadow">
+                    <label className="text-xs uppercase tracking-wide">Sevirity</label>
+                    <div className="flex gap-2 mt-2">
+                        {severities.map(level =>(
+                            <button
+                                key={level}
+                                onClick={() => handleChange('severity',level)}
+                                className={`flex-1 py-2 rounded-lg text-sm font-medium border 
+                                ${form.severity === level ? 
+                                    level === 'Minor' 
+                                    ? 'bg-yellow-400 text-white border-yellow-400' 
+                                    : level === 'Major' 
+                                        ? 'bg-orange-500 text-white border-orange-500' 
+                                        : 'bg-red-600 text-white border-red-600' 
+                                    : 'bg-white text-gray-400 border-gray-200'
+                                }`}>
+                                {level}
+                                </button>
+
+                        ))}
+                    </div>
+                </div>
+
+                {/*Barangays*/}
+                <div className="bg-white rounded-xl p-4 shadow">
+                    <label className="text-xs uppercase tracking-wide">
+                        Barangay
+                    </label>
+                    <select className="w-full mt-2 p-2 border border-gray-200 rounded-lg text-sm"
+                        value={form.barangay}
+                        onChange={e => handleChange('barangay',e.target.value)} >
+                        <option value="">Select barangay...</option>
+                        {barangays.map(brgy =>(
+                            <option key={brgy} value={brgy} >{brgy}</option>
+                            ))}
+                        
+                    </select>
+                </div>
+                {/*Incident Description*/}
+                <div className="bg-white rounded-xl p-4 shadow">
+                        <label className="text-xs uppercase tracking-wide">Description</label>
+                        <textarea className="w-full mt-2 p-2 border border-gray-200 rounded-lg text-sm"
+                            value={form.description} 
+                            placeholder="Describe affected area..." 
+                            onChange={e => handleChange('description', e.target.value)}
+
+                        />
+
+
+                </div>
+
+                {/*Incident radius*/}
+                <div className="bg-white rounded-xl p-4 shadow">
+                    <label className="text-xs uppercase tracking-wide">Estimated affected area</label>
+                    <div className="flex gap-2 mt-2">
+                    {['50m','100m','500m','1km'].map(r =>
+                        <button
+                            key={r} 
+                            onClick={() => handleChange('radius',r)}
+                            className={`flex-1 py-2 rounded-lg text-sm font-medium border
+                                ${form.radius === r
+                                 ? 'bg-red-700 text-white border-red-700' 
+                                 : 'bg-white text-gray-400 border-gray-200'
+                            }`}>
+                        {r}</button>
+
+                        )}
+                        
+                    </div>
+                </div>
+
+                {/* GPS Location */}
+                <div className="bg-white rounded-xl p-4 shadow">
+                    <label className="text-xs text-gray-500 uppercase tracking-wide">GPS Location</label>
+                    <button
+                        onClick={geoLocation} 
+                        className="w-full mt-2 gap-2 bg-gray-100  border border-gray-200 rounded-lg text-sm  text-gray-500 items-center justify-center gap-2">
+                        <span>📍</span>
+                        {form.latitude ? `${form.latitude}, ${form.longitude}` : 'Tap to capture location'}
+                        </button>
+                </div>
+                {/*Handle submit*/}
+                <button onClick={handleSubmit} 
+                className="w-full py-4 bg-red-700 text-white  rounded-lg font-bold text-lg">Submit</button>
+
             </div>
         </MobileLayout>
     );
